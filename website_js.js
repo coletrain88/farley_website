3dcc672d330c2783abbc9c90057d6c3f485f48fb
coletrain88 <162764609+coletrain88@users.noreply.github.com>
2024-05-27 22:20:04 -0400

    Update website_js.js

--- website_js.js
+++ website_js.js
@@ -1,5 +1,5 @@
 document.getElementById('muffinButton').addEventListener('click', function() {
     var muffinImage = document.getElementById('muffinImage');
-    muffinImage.src = "muffin.jpg";
+    muffinImage.src = "images/muffin.jpg";
     muffinImage.style.display = "block";
 });
