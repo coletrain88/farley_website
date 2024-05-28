ad8c6d7c0b70d66bccd3f38587a7260661d54c2e
coletrain88 <162764609+coletrain88@users.noreply.github.com>
2024-05-27 22:03:53 -0400

    Add files via upload

---
+++ website_js.js
@@ -0,0 +1,5 @@
+document.getElementById('muffinButton').addEventListener('click', function() {
+    var muffinImage = document.getElementById('muffinImage');
+    muffinImage.src = "file:///C:/Users/colto/Desktop/muffin.jpeg";
+    muffinImage.style.display = "block";
+});
